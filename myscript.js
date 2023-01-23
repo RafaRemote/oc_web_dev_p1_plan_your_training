e3064d797fbd703d4195105f948c7f3887dce6fb
rafaRemote <57028375+RafaRemote@users.noreply.github.com>
2023-01-23 16:07:24 +0100

    MODIFIES: fetch different source than before

--- myscript.js
+++ myscript.js
@@ -34,18 +34,17 @@ if (document.title == html_page_titles["job"]) {
         }
     };
 
-
     fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/EUR', options)
         .then(response => response.json())
         .then((data) => {
             rate_euro_pln = data.rates['PLN']
             console.log("1 euro = ", rate_euro_pln, "złotych. source=https://exchangerate-api.p.rapidapi.com/rapid/latest/EUR")
-            fetch('https://www.insee.fr/fr/statistiques/1375188', {mode: 'no-cors'})
+            fetch('https://www.service-public.fr/particuliers/vosdroits/F2300')
             .then(res => res.text())
             .then(html => {
                 var parser = new DOMParser()
                 var doc = parser.parseFromString(html, 'text/html')
-                console.log("hourly wage in France, brutto: ", Number(doc.getElementsByTagName("td")[0].innerText.replace(',', '.')), "euros. Source=https://www.insee.fr/fr/statistiques/1375188")
+                console.log("hourly wage in France, brutto: ", Number(doc.getElementsByClassName("sp-prix")[0].innerText.replace(',', '.')), "euros. Source=https://www.insee.fr/fr/statistiques/1375188")
                 var wage = Math.round(Number(doc.getElementsByTagName("td")[0].innerText.replace(',', '.'))*151.67*rate_euro_pln)
                 document.getElementById("realistic_wage").innerText = wage;
             })
